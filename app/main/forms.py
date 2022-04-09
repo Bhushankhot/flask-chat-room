7a31ae7d694e9b5544209af8e72c54741e30fed7
“Bhushan08” <bhushan.khot22@gmail.com>
2022-04-09 12:29:39 +0530

    Flask chat 1st commit

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,5 +1,7 @@
 from flask_wtf import Form
-from wtforms.fields import StringField, SubmitField
+from wtforms.fields import StringField, SubmitField, TextField, BooleanField, PasswordField, TextAreaField
+from wtforms import TextField, BooleanField, PasswordField, TextAreaField, validators, StringField
+from wtforms.validators import DataRequired
 from wtforms.validators import Required
 
 
